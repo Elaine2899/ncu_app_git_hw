f8182d24d969594bd6616b97a2f1d4584c951714
杜羽 <90547641+xCk27x@users.noreply.github.com>
2024-05-10 12:51:57 +0800

    Update test.ts
    add function sub10

--- test.ts
+++ test.ts
@@ -2,6 +2,12 @@ function add10 (a: number): number {
   return a += 10;
 }
 
-let b = add10(2);
+function sub10 (a: number) {
+  return a -= 10;
+}
 
-console.log(b);
+let b = add10(2);
+let c = sub10(3);
+
+console.log('b = ', b);
+console.log('c = ', c);
